fbf4af7556d83505b7b880a7db5fc66f190d562a
bank <watchira_5@hotmail.com>
2023-06-20 22:27:37 +0700

    add homepage ui and navbar

--- elysainFrontend/src/App.jsx
+++ elysainFrontend/src/App.jsx
@@ -1,11 +1,5 @@
-import { useState } from 'react'
+import Router from "./route/Router";
 
-function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+export default function App() {
+  return <Router />;
 }
-
-export default App
