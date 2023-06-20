b055a7eb817d3c26244441ad146aa2f08b3dc467
“suthee17725” <sute17725@gmail.com>
2023-06-20 15:03:08 +0700

    add auth0

--- elysainFrontend/src/App.jsx
+++ elysainFrontend/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
+import LoginButton from "./features/auth/components/LoginButton";
+import LogoutButton from "./features/auth/components/LogoutButton";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <LoginButton />
+      <LogoutButton />
+    </>
+  );
 }
 
-export default App
+export default App;
