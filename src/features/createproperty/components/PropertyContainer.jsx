6c23074c7b4d23aba1ca1e0751f685380f69046b
“suthee17725” <sute17725@gmail.com>
2023-06-23 21:49:11 +0700

    Create Property

--- src/features/createproperty/components/PropertyContainer.jsx
+++ src/features/createproperty/components/PropertyContainer.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import HeaderAgent from '../../../components/HeaderAgent'
-import AminityForm from './AminityForm'
-import PropertyForm from './PropertyForm'
+import React from "react";
+import HeaderAgent from "../../../components/HeaderAgent";
+import AminityForm from "./AminityForm";
+import PropertyForm from "./PropertyForm";
+import PropertyEditForm from "./PropertyEditForm";
 
 export default function PropertyContainer() {
   return (
     <>
-        <HeaderAgent topic="Create Property"/>
-        <div className='flex flex-col gap-6 m-8 mt-0'>
-            <PropertyForm />
-        </div>
+      <HeaderAgent topic="Create Property" />
+      <div className="flex flex-col gap-6 m-8 mt-0">
+        <PropertyForm />
+      </div>
+      <HeaderAgent topic="Edit Property " />
+      <div className="flex flex-col gap-6 m-8 mt-0">
+        <PropertyEditForm />
+      </div>
     </>
-  )
+  );
 }
