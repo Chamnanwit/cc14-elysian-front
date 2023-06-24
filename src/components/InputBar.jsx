4bbe14b936076ad0cb43b414181c2e41c6442093
bank <watchira_5@hotmail.com>
2023-06-24 16:27:46 +0700

    backup

--- src/components/InputBar.jsx
+++ src/components/InputBar.jsx
@@ -12,7 +12,7 @@ export default function InputBar({
     `px-5 py-1 border-none text-lg rounded-full w-full ring-2 outline-none focus:ring-2 transition-all duration-200`,
     isInvalid
       ? "border-red-500 focus:ring-red-300"
-      : "border-gray-300 focus:border-blue-500 focus:ring-blue-300  w-full",
+      : "border-gray-300 focus:border-blue-500 focus:ring-blue-300 w-full",
     disabled ? "bg-gray-100" : "bg-white"
   );
   return (
