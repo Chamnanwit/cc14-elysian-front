92770b6074924c1e3b87c78578e7c8e7b7fb8474
ChamnanwitEamdit <Chamnanwiteamdit@gmail.com>
2023-06-27 21:32:54 +0700

    edit config

--- src/features/adminInformation/components/AdminContainer.jsx
+++ src/features/adminInformation/components/AdminContainer.jsx
@@ -3,7 +3,6 @@ import HeaderAdmin from "../../../components/HeaderAdmin";
 import InputForm from "../../../components/InputForm";
 import AdminItem from "./AdminItem";
 import { useState } from "react";
-<<<<<<< HEAD
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import {
@@ -11,6 +10,7 @@ import {
   searchAdminInformationAsync,
   setSearchValueRedux,
 } from "../slice/admin-slice";
+import AdminForm from "./AdminForm";
 
 export default function AdminContainer() {
   // const [isAddMode, setIsAddMode] = useState(false);
@@ -46,16 +46,6 @@ export default function AdminContainer() {
   return (
     <>
       <HeaderAdmin topic="Admin List" />
-=======
-import { useDispatch } from "react-redux";
-import AdminForm from "./AdminForm";
-
-export default function AdminContainer({ data }) {
-  const [isAddMode, setIsAddMode] = useState(false);
-  const dispatch = useDispatch();
-  return (
-    <>
-      <HeaderAdmin topic="Admin List" />
       {isAddMode? (
         <div className="m-8 mt-0">
           <AdminForm
@@ -72,13 +62,10 @@ export default function AdminContainer({ data }) {
           เพิ่ม
         </button>
       )}
-
->>>>>>> 3576d8445a0dab8bf5876a67ca107502f2df6554
       <div className="bg-white rounded-md m-8 px-8 pt-6 pb-8 mt-0">
         <div className="flex items-baseline gap-4 justify-end mb-6">
           <div>ค้นหา:</div>
           <div className="100px text-md">
-<<<<<<< HEAD
             <InputForm
               type="text"
               className="header__search__input"
@@ -86,9 +73,6 @@ export default function AdminContainer({ data }) {
               onChange={handleChange}
               value={searchValue}
             />
-=======
-            <InputForm />
->>>>>>> 3576d8445a0dab8bf5876a67ca107502f2df6554
           </div>
         </div>
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg text-lg">
@@ -102,11 +86,7 @@ export default function AdminContainer({ data }) {
                   ชื่อ
                 </th>
                 <th scope="col" class="px-6 py-5">
-<<<<<<< HEAD
                   อีเมล
-=======
-                  Email
->>>>>>> 3576d8445a0dab8bf5876a67ca107502f2df6554
                 </th>
                 <th scope="col" class="px-6 py-5">
                   สถานะ
@@ -117,11 +97,7 @@ export default function AdminContainer({ data }) {
               </tr>
             </thead>
             <tbody>
-<<<<<<< HEAD
               {adminInformationArr.map((el) => (
-=======
-              {data.map((el) => (
->>>>>>> 3576d8445a0dab8bf5876a67ca107502f2df6554
                 <AdminItem key={el.id} el={el} />
               ))}
             </tbody>
--- src/features/adminInformation/components/AdminItem.jsx
+++ src/features/adminInformation/components/AdminItem.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { TrashIcon } from "../../../icons";
-<<<<<<< HEAD
 import { HiEye } from "react-icons/hi";
 import Modal from "../../../components/Modal";
 import { useDispatch, useSelector } from "react-redux";
@@ -136,84 +135,5 @@ export default function AdminItem({ el }) {
         <></>
       )}
     </>
-=======
-
-import { HiEye } from "react-icons/hi"
-import Modal from "../../../components/Modal";
-
-export default function AdminItem({ el }) {
-  const [isViewMode, setIsViewMode] = useState(false);
-  const [status, setStatus] = useState(el.status)
-  console.log(status)
-  return (
-    <>
-        <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                <th
-                  scope="row"
-                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {el.id}
-                </th>
-                <td class="px-6 py-4">{el.firstName} {el.lastName}</td>
-                <td class="px-6 py-4">{el.email}</td>
-                <td class="px-6 py-4">
-                    <label class="relative inline-flex items-center cursor-pointer">
-                        {(status)? <input type="checkbox" value="" className="sr-only peer" checked onClick={() => {status? setStatus(0): setStatus(1)}}/> : <input type="checkbox" value="" className="sr-only peer" onClick={() => {status? setStatus(0): setStatus(1)}}/>  }
-                        <div className="w-11 h-6 bg-red-700 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600 tra"></div>
-                    </label>
-                </td>
-                
-                <td class="px-6 py-4 flex gap-3">
-                    <div
-                        className="bg-blue-700 p-[7px] rounded-md cursor-pointer flex items-center"
-                        onClick={(e) => {
-                        e.stopPropagation();
-                        setIsViewMode(true);
-                        }}
-                    >
-                        <HiEye fill="#ffffff"/>
-                    </div>
-                    <div className="bg-red-700 p-[5px] rounded-md cursor-pointer">
-                        <TrashIcon />
-                    </div>
-                </td>
-        </tr>
-        {isViewMode? 
-        <Modal title="ประวัติ" width="50" open={isViewMode} onClose={()=> setIsViewMode(false)}>
-        <table className="border w-full border-collapse">
-          {/* <tr>
-            <td className="w-1/2 px-3 py-2 border-b border-r">รูปภาพ</td>
-            <td className="w-1/2 p-3 border-b">{el.agent}</td>
-          </tr> */}
-          <tr>
-            <td className="w-1/2 px-3 py-2 border-b border-r">ชื่อ-นามสกุล</td>
-            <td className="w-1/2 p-3 border-b">{el.firstName} {el.lastName}</td>
-          </tr>
-          <tr>
-            <td className="w-1/2 px-3 py-2 border-b border-r">อีเมล</td>
-            <td className="w-1/2 p-3 border-b">{el.email}</td>
-          </tr>
-          <tr>
-            <td className="w-1/2 px-3 py-2 border-b border-r">เบอร์โทรศัพท์</td>
-            <td className="w-1/2 p-3 border-b"> {el.phone}</td>
-          </tr>
-          <tr>
-            <td className="w-1/2 px-3 py-2 border-b border-r">บัตรประชาชน</td>
-            <td className="w-1/2 p-3 border-b"> {el.cardid}</td>
-          </tr>
-          <tr>
-            <td className="w-1/2 px-3 py-2 border-b border-r">สถานะ</td>
-            <td className="w-1/2 p-3 border-b">
-                <label class="relative inline-flex items-center cursor-pointer">
-                    {(status)? <input type="checkbox" value="" className="sr-only peer" checked/> : <input type="checkbox" value="" className="sr-only peer-disabled"/>  }
-                    <div className="w-11 h-6 bg-red-700 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
-                </label>
-            </td>
-          </tr>
-        </table>
-        </Modal>
-      : <></>}
-      </>
->>>>>>> 3576d8445a0dab8bf5876a67ca107502f2df6554
   );
 }
