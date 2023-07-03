4b7a488f1c7f9db94c64917758c059e923cf6752
“suthee17725” <sute17725@gmail.com>
2023-07-03 16:55:04 +0700

    fix1

--- src/features/createproperty/components/EditorForm.jsx
+++ src/features/createproperty/components/EditorForm.jsx
@@ -1,4 +1,5 @@
 import { Editor } from "@tinymce/tinymce-react";
+import useRef from "react";
 
 export default function EditorForm({ value, onChange, isInvalid }) {
   return (
@@ -8,7 +9,44 @@ export default function EditorForm({ value, onChange, isInvalid }) {
       // initialValue="<p>This is the initial content of the editor.</p>"
       init={{
         height: 500,
-        menubar: false,
+        menubar: {
+          file: {
+            title: "File",
+            items:
+              "newdocument restoredraft | preview | export print | deleteallconversations",
+          },
+          edit: {
+            title: "Edit",
+            items:
+              "undo redo | cut copy paste pastetext | selectall | searchreplace",
+          },
+          view: {
+            title: "View",
+            items:
+              "code | visualaid visualchars visualblocks | spellchecker | preview fullscreen | showcomments",
+          },
+          insert: {
+            title: "Insert",
+            items:
+              "image link media addcomment pageembed template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor tableofcontents | insertdatetime",
+          },
+          format: {
+            title: "Format",
+            items:
+              "bold italic underline strikethrough superscript subscript codeformat | styles blocks fontfamily fontsize align lineheight | forecolor backcolor | language | removeformat",
+          },
+          tools: {
+            title: "Tools",
+            items:
+              "spellchecker spellcheckerlanguage | a11ycheck code wordcount",
+          },
+          table: {
+            title: "Table",
+            items:
+              "inserttable | cell row column | advtablesort | tableprops deletetable",
+          },
+          help: { title: "Help", items: "help" },
+        },
         plugins: [
           "advlist",
           "autolink",
